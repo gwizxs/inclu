71d33b0dba815e66ccc6710d0b2340f887b8c38a
gwizxs <gwizxs@gmail.com>
2024-11-04 14:14:52 +0300

    configured webpack-dev-server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,29 @@
 import path from 'path';
 import webpack from 'webpack';
-import { BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 
-
+export default (env: BuildEnv) => {
 const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
 }
 
-const mode = 'development';
+const mode = env.mode || 'development';
+const PORT = env.port || 3000;
+
+
 const isDev = mode === 'development';
+
+
 const config: webpack.Configuration = buildWebpackConfig({
-    mode: 'development',
+    mode,
     paths,
     isDev,
+    port: PORT,
 })
 
-export default config;
+
+    return config
+};
