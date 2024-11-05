0d8694c54a6ba60ba2e25903c8e885342bc72b56
gwizxs <gwizxs@gmail.com>
2024-11-05 18:41:19 +0300

    add theme in site

--- src/Routes/AppRouter.tsx
+++ src/Routes/AppRouter.tsx
@@ -2,21 +2,24 @@ import { Link, Route, Routes } from "react-router-dom";
 import { MainPageAsync } from "../pages/Main/Main.async";
 import { AboutPageAsync } from "../pages/About/About.async";
 import { Suspense } from "react";
-
-
-
+import '../styles/index.scss'
+import { useTheme } from "../theme/useTheme";
 
 export const AppRouter = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div>
-            <Link to={'/'}>main</Link>
-            <Link to={'/about'}>about</Link>
+        <div className={`app ${theme || 'normal'}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={"/about"} element={<AboutPageAsync />}></Route>
-                    <Route path={"/"} element={<MainPageAsync />}></Route>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
                 </Routes>
             </Suspense>
         </div>
     );
 };
+
