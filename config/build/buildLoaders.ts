6d77eed6ca6fecbd4d8235f2e9fc7db7227f735d
gwizxs <gwizxs@gmail.com>
2024-11-07 21:00:19 +0300

    add photo loader"

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -33,8 +33,19 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const filesLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
     return [
         typescriptLoader,
         cssLoader,
+        svgLoader,
+        filesLoader
     ]
 }
