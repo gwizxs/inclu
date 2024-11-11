c03b1647ba37f7f0e07d6092eca74d026af7609e
gwizxs <gwizxs@gmail.com>
2024-11-11 18:34:28 +0300

    add style lint and fix styles

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,6 +1,5 @@
 
 import { classNames } from "shared/lib/classNames";
-import s from './ThemeSwitcher.module.scss'
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
 import { Theme, useTheme } from "app/providers/ThemeProvider";
@@ -17,7 +16,7 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps  ) => {
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(s.ThemeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
             {theme === Theme.DARK ? <LightIcon /> : <DarkIcon />}
