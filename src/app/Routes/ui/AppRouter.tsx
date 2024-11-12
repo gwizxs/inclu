ea15cd5fb5fbe5cf58824980ee7a8a5c069a11fd
gwizxs <gwizxs@gmail.com>
2024-11-12 20:52:30 +0300

    add 404 page

--- src/app/Routes/ui/AppRouter.tsx
+++ src/app/Routes/ui/AppRouter.tsx
@@ -1,27 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
 import { routeConfig } from "shared/config/routerConfig/routerConfig";
+import { PageLoader } from "widgets/PageLoader";
 
-export const AppRouter = () => {
+export const AppRouter = () => (
+    <Routes>
+        {Object.entries(routeConfig).map(([path, { element }]) => (
+            <Route
+                key={path}
+                path={path}
+                element={
+                    <Suspense fallback={<PageLoader />}>
+                        <div className="page-wrapper">
+                            {element}
+                        </div>
+                    </Suspense>
+                }
+            />
+        ))}
+    </Routes>
+);
 
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.entries(routeConfig).map(([path, {element}]) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={(
-                            <div className="page-wrapper">
-                                {element}
-                            </div>
-                        )}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
-    );
-};
-
-export default AppRouter
+export default AppRouter;
 
