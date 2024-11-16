2d22c537be3bf28532719fcdb93373b2b49f87c6
gwizxs <gwizxs@gmail.com>
2024-11-16 16:17:03 +0300

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslint from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
+    eslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
     pluginReact.configs.flat.recommended,
     {
         settings: {
