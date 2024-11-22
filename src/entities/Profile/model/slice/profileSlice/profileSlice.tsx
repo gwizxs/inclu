9d7a294fd84f7bdca5b6aca7e593e439af6d6a5b
gwizxs <gwizxs@gmail.com>
2024-11-22 18:23:27 +0300

    add profile

---
+++ src/entities/Profile/model/slice/profileSlice/profileSlice.tsx
@@ -0,0 +1,20 @@
+import {  ProfileSchema } from '../../types/Profile';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState: ProfileSchema = {
+    readonly: true,
+    isLoading: false,
+    error: undefined,
+    data: undefined,
+};
+
+export const profileSlice = createSlice({
+    name: 'profile',
+    initialState,
+    reducers: {
+    },
+});
+
+// Action creators are generated for each case reducer function
+export const { actions: profileActions } = profileSlice;
+export const { reducer: profileReducer } = profileSlice;
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,16 +1,23 @@
 
 
+import { profileReducer } from "entities/Profile";
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
+import { DynamicModuleLoader, ReducersList } from "shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
 
+const reducers: ReducersList = {
+    profile: profileReducer
+}
 
 export const ProfilePage = memo(() => {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     return (
-        <div>
-            {t('Профиль')}
-        </div>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div>
+                {t('Профиль')}
+            </div>
+        </DynamicModuleLoader>
     )
 })
 
