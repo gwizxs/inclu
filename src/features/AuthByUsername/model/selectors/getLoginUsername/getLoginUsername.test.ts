30e4b01b797a81ad3b71d47b1cd0461571acf3e8
gwizxs <gwizxs@gmail.com>
2024-11-22 15:12:42 +0300

    add unit test in selectors

---
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -0,0 +1,17 @@
+import { DeepPartial } from "@reduxjs/toolkit";
+import { StateSchema } from "app/providers/StoreProvider";
+import { getLoginUsername } from "./getLoginUsername";
+
+
+describe('getLoginLoading.test', () => {
+    test('should return username', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: { username: 'user' },
+        };
+        expect(getLoginUsername(state as StateSchema)).toEqual('user');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginUsername(state as StateSchema)).toEqual('');
+    });
+});
