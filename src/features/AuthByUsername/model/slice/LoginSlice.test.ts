fc042e7384c30732084afee0712ad1baef06985a
gwizxs <gwizxs@gmail.com>
2024-11-22 16:12:11 +0300

    add test in slice

---
+++ src/features/AuthByUsername/model/slice/LoginSlice.test.ts
@@ -0,0 +1,21 @@
+import { loginActions, loginReducer } from "./LoginSlice";
+import { DeepPartial } from "@reduxjs/toolkit";
+import { LoginSchema } from "../types/loginSchema";
+
+describe('LoginSlice.test', () => {
+    test('test set Username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123'};
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('123')
+        )).toBe({ username: '123' });
+    })
+
+    test('test set Password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123'};
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('123132')
+        )).toBe({ username: '123132' });
+    })
+})
