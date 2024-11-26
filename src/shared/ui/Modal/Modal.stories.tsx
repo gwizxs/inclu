28a9605981cd99f2f84569676e3796955cdc2bc7
gwizxs <gwizxs@gmail.com>
2024-11-26 17:42:00 +0300

    fix story in modal

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Modal } from 'shared/ui/Modal/Modal';
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Modal } from './Modal';
 import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
-export default {
+const meta: Meta<typeof Modal> = {
     title: 'shared/Modal',
     component: Modal,
-    argTypes: {
-        backgroundColor: { control: 'color' },
+};
+
+export default meta;
+type Story = StoryObj<typeof Modal>;
+
+
+
+export const Primary: Story = {
+    args: {
+        isOpen: true,
+        children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
+    }
+}
+
+export const Dark: Story = {
+    args: {
+        isOpen: true,
+        children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
     },
-} as ComponentMeta<typeof Modal>;
-
-const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-    isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
-};
-
-export const Dark = Template.bind({});
-Dark.args = {
-    isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
-};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+    decorators: [
+        ThemeDecorator(Theme.DARK)
+    ]
+}
