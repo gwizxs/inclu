d0764f93d833a83d6aa1954129a7202de8801474
gwizxs <gwizxs@gmail.com>
2024-12-03 17:08:59 +0300

    add tests in selectors profile

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -24,4 +24,4 @@ export {getProfileError} from './model/selectors/getProfileError/getProfileError
 export {getProfileReadonly} from './model/selectors/getProfileReadonly/getProfileReadonly';
 export {getProfileForm} from './model/selectors/getProfileForm/getProfileForm';
 export {updateProfileData} from './model/services/updateProfileData/updateProfileData';
-export {getProfileValidateError} from './model/selectors/getProfileValidateError/getProfileValidateError';
+export {getProfileValidateError} from './model/selectors/getProfileValidateErrors/getProfileValidateErrors'
