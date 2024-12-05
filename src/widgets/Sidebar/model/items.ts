e04b8454e40f271f02b9bb7fff66e51f03aef852
gwizxs <gwizxs@gmail.com>
2024-12-05 16:57:45 +0300

    add items in sidebar and fix path

--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -3,6 +3,8 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
+import ArticlesIcon from 'shared/assets/icons/article-20-20.svg';
+
 
 export interface SidebarItemType {
     path: string;
@@ -28,4 +30,10 @@ export const SidebarItemsList: SidebarItemType[] = [
         text: 'Профиль',
         authOnly: true,
     },
+    {
+        path: RoutePath.articles,
+        Icon: ArticlesIcon,
+        text: 'Статьи',
+        authOnly: true
+    }
 ];
