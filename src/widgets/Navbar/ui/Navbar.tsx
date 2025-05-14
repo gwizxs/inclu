7a9e9df255d6075f5715fd5c856fb7a1666e1f8c
gwizxs <gwizxs@gmail.com>
2025-05-14 21:07:39 +0300

    add e2e tests and add tests in router

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -6,7 +6,7 @@ import { LoginModal } from '@/features/AuthByUsername';
 import {
     getUserAuthData,
 } from '@/entities/User';
-import { Text, TextTheme } from '@/shared/ui/Text';
+import { Text } from '@/shared/ui/Text';
 import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink';
 
 import classNames from '@/shared/library/ClassNames/classNames';
@@ -15,6 +15,7 @@ import { NotificationsButton } from '@/features/NotificationsButton';
 import { AvatarDropdown } from '@/features/AvatarDropdown';
 import cls from './Navbar.module.scss';
 import { getRouteArticleCreate } from '@/shared/const/router';
+import { TextTheme } from '@/shared/ui/Text/Text';
 interface NavbarProps {
     className?: string;
 }
