9f48fd93fad8bd58ef4b1fa894f017d3e8b32c36
gwizxs <gwizxs@gmail.com>
2025-05-27 16:21:40 +0300

    ADD ISOLATED TESTS

---
+++ cypress/component/EditableProfileCard.cy.tsx
@@ -0,0 +1,27 @@
+import { EditableProfileCard } from "@/features/editableProfileCard"
+import { TestProvider } from "@/shared/library/tests/componentRender/componentRender"
+
+
+const USER_ID = '1'
+
+
+describe('EditableProfileCard.cy.tsx', () => {
+  it('playground', () => {
+    cy.intercept('GET', '**/profile/*', { fixture: 'profile.json' })
+    cy.mount(
+      <TestProvider options={{
+        initialState: {
+          user: {
+            authData: {
+              id: USER_ID,
+              username: 'admin',
+              avatar: 'https://x.com/i/web/status/1732641424130439168'
+            }
+          }
+        }
+      }}>
+        <EditableProfileCard id={USER_ID} />
+      </TestProvider>
+    )
+  })
+})
